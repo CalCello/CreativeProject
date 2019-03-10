909178a51af1e25365fca15d20684dea5ae41bba
Cal <caleblewis3@gmail.com>
2019-03-10 13:29:44 -0600

    added basic game functionality for wordgame

--- scripts/wordgame.js
+++ scripts/wordgame.js
@@ -3,6 +3,9 @@ let app = new Vue({
   el: '#app',
   data: {
     gameID: 'wordgame',
+    prevWord: '',
+    word: '',
+    addedWords: [],
     addedName: '',
     addedComment: '',
     commentTime: '',
@@ -10,7 +13,68 @@ let app = new Vue({
     ratings: {},
     averageRatings: '',
   },
+  computed: {
+    allWords() {
+      return this.addedWords;
+    }
+  },
   methods: {
+    addWord() {
+      var newWordIter = 0, prevWordIter = 0;
+      var newWordLen = this.word.length;
+      var prevWordLen = this.prevWord.length;
+      var edits = 0;
+      //First word entered
+      if(this.prevWord === '') {
+        console.log("FIRST");
+        this.addedWords.push(this.word);
+        this.prevWord = this.word;
+        this.word = '';
+        return;
+      }
+
+      // Credit GeeksforGeeks for solution
+      while(newWordIter < newWordLen && prevWordIter < prevWordLen) {
+        if(this.word.charAt(newWordIter) != this.prevWord.charAt(prevWordIter)) {
+          edits++;
+          if(newWordLen > prevWordLen) {
+            newWordIter++;
+          }
+          else if(prevWordLen > newWordLen) {
+            prevWordIter++;
+          }
+          else {
+            newWordIter++;
+            prevWordIter++;
+          }
+        }
+        else {
+          newWordIter++;
+          prevWordIter++;
+        }
+      }
+
+      if(newWordIter < newWordLen || prevWordIter < prevWordLen) {
+        edits++;
+      }
+
+      // You changed the word properly
+      if(edits <= 1) {
+        console.log("RIGHT");
+        this.addedWords.push(this.word);
+        this.prevWord = this.word;
+        this.word = '';
+      }
+      // You failed to change the word properly
+      else {
+        console.log("WRONG");
+        alert("You got a score of " + this.addedWords.length);
+        this.addedWords.length = 0;
+        this.prevWord = '';
+        this.word = '';
+      }
+    },
+
     setRating(rating){
       if (!(this.gameID in this.ratings))
         Vue.set(this.ratings, this.gameID, {
@@ -22,6 +86,7 @@ let app = new Vue({
       this.ratings[this.gameID].total += 1;
       this.averageRatings = (this.ratings[this.gameID].sum / this.ratings[this.gameID].total);
      },
+
      addComment() {
        if (!(this.gameID in this.comments))
          Vue.set(app.comments, this.gameID, new Array);
@@ -33,5 +98,6 @@ let app = new Vue({
        this.addedName = '';
        this.addedComment = '';
      },
+
   }
 });
